fcff3991ccd1a39cf93ad9956bf75cfec6f281ba
Fayas <TheFayasNoushad@gmail.com>
2021-04-19 07:33:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,21 +11,75 @@ from telegraph import upload_file
 
 FayasNoushad = Client("Telegraph Uploader Bot", bot_token = os.environ["BOT_TOKEN"], api_id = int(os.environ["API_ID"]), api_hash = os.environ["API_HASH"])
 
-@FayasNoushad.on_message(filters.command(["start"]))
-async def start(bot, update):
-    text = f"""
-Hello {update.from_user.mention}, I am small media or file to telegra.ph link uploader bot.
-
+START_TEXT = """
+Hello {}, I am an under 5MB media or file to telegra.ph link uploader bot.
+"""
+HELP_TEXT = """
 - Just give me a media under 5MB
 - Then I will download it
 - I will then upload it to the telegra.ph link
 """
-    reply_markup=InlineKeyboardMarkup(
+ABOUT_TEXT = """
+Bot : [Telegraph Uploader](https://telegram.me/FnTelegraphBot)
+Creator : [Fayas](https://telegram.me/TheFayas)
+Channel : [Fayas Noushad](https://telegram.me/FayasNoushad)
+Source : [Click here](https://github.com/FayasNoushad/Telegraph-Uploader-Bot)
+Language : [Python3](https://python.org)
+Library : [Pyrogram v1.2.0](https://pyrogram.org)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
         InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
         ]]
     )
+
+@FayasNoushad.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    else:
+        await update.message.delete()
+    
+
+@FayasNoushad.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
     await bot.send_message(
         chat_id=update.chat.id,
         text=text,
@@ -37,22 +91,37 @@ Hello {update.from_user.mention}, I am small media or file to telegra.ph link up
 @FayasNoushad.on_message(filters.media & filters.private)
 async def getmedia(bot, update):
     medianame = "./DOWNLOADS/" + "FayasNoushad/FnTelegraphBot"
-    text = await bot.send_message(
+    message = await bot.send_message(
         chat_id=update.chat.id,
         text="<code>Downloading to My Server ...</code>",
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
-    await bot.download_media(message=update, file_name=medianame)
-    await text.edit_text(text="<code>Downloading Completed. Now I'am Uploading to telegra.ph Link ...</code>")
+    await bot.download_media(
+        message=update,
+        file_name=medianame
+    )
+    await message.edit_text(
+        text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>"
+    )
     try:
         response = upload_file(medianame)
     except Exception as error:
         print(error)
-        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)
+        text=f"Error :- <code>{error}</code>"
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('More Help', callback_data='help')
+            ]]
+        )
+        await message.edit_text(
+            text=text,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
         return
-    text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad",
+    text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad"
     reply_markup=InlineKeyboardMarkup(
         [[
         InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
@@ -61,7 +130,7 @@ async def getmedia(bot, update):
         InlineKeyboardButton(text="Join Updates Channel", url="https://telegram.me/FayasNoushad")
         ]]
     )
-    await text.edit_text(
+    await message.edit_text(
         text=text,
         disable_web_page_preview=True,
         reply_markup=reply_markup
