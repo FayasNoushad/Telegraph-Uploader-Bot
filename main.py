4234fb300f26222424499a209234d382b3a3feea
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2023-06-21 21:15:35 +0530

    Fix

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def getmedia(bot, update):
     medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
     
     try:
-        message = await update.reply_message(
+        message = await update.reply_text(
             text="`Processing...`",
             quote=True,
             disable_web_page_preview=True
