d14aee27529465a0a7e82cd1447490363c6f9bf3
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2024-08-31 09:44:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     BOT_TOKEN = os.environ.get("7339389992:AAEUMjPbcfmh0GRLGlZkDBn8-8uRz8FUKHg")
-    API_ID = int(os.environ.get("28271161"))
+    API_ID = int(os.environ.get('28271161'))
     API_HASH = os.environ.get("c5598a74beecfb1d1be93ae85347b367")
